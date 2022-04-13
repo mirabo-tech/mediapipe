8766a55cda38eb5e7aec7c303357b6f2a347e768
phuclm <phuclm@mirabo.com.vn>
2022-04-13 18:32:20 +0700

    object detection from file

--- build_object_detection_from_file.sh
+++ build_object_detection_from_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 --define no_aws_support=true --linkopt=-s \
-mediapipe/examples/desktop/object_detection:object_detection_tensorflow
+bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 \
+mediapipe/examples/desktop/object_detection:object_detection_tflite
 
-GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection/object_detection_tensorflow \
-  --calculator_graph_config_file=mediapipe/graphs/object_detection/object_detection_desktop_tensorflow_graph.pbtxt \
-  # --input_side_packets=input_video_path=/home/phuclm/video.mp4,output_video_path=/home/phuclm/video-w-obj.mp4
+GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection/object_detection_tflite   \
+--calculator_graph_config_file=mediapipe/graphs/object_detection/object_detection_desktop_tflite_graph.pbtxt \
+--input_side_packets=input_video_path=/home/phuclm/video.mp4,output_video_path=/home/phuclm/video-w-labale.mp4
